ea18b0012e613f2754cb42256d77efe32dd81d5c
Gabin PAQUES <106560023+gabpak@users.noreply.github.com>
2023-11-08 15:27:12 +0100

    Adding the rectangular signal

--- main.c.asm
+++ main.c.asm
@@ -22,8 +22,11 @@ DIVISION_NUMERATOR	equ 0x0A ; The numerator of the division
 DIVISION_DENOMINATOR	equ 0x0B ; The denominator of the division
 DIVISION_RESULT		equ 0x0C ; The result of the division
 DIVISION_MODULO		equ 0x0D ; The rest of the euclidian division
-		
+	
 NUMBER_7_SEGMENTS	equ 0x10 ; The number to display on the 7-segments
+	
+ADRESSH_TO_DECREMENT	equ 0x20 ; The variable to decrement for the rectangular signal
+RECTANGLE_HIGH		equ 0x21	
 		
 		
 ORG 0x0000
@@ -36,14 +39,14 @@ INIT
     MOVWF TRISA
     CLRF PORTA ; Cleaning the 7-segments/transistors port A
 
-    CLRF PORTB ; Affichage du resultat A/D sur le port C
     CLRF TRISB
-    
-    CLRF PORTC ; Affichage du resultat A/D sur le port C
-    CLRF TRISC
+    CLRF PORTB ; Affichage du resultat A/D sur le port C
 
+    CLRF TRISD ; Utilisé pour l'affichage du 7 segment
     CLRF PORTD  
-    CLRF TRISD
+    
+    CLRF TRISE ; Utilisé pour le signal rectangle
+    CLRF PORTE
 
     ; Configuration of the AD Interrupts / ADCON0
     MOVLW b'00010001'
@@ -72,8 +75,10 @@ INIT
     CLRF DIVISION_DENOMINATOR
     CLRF DIVISION_RESULT
     CLRF DIVISION_MODULO
+    
+    CLRF ADRESSH_TO_DECREMENT
+    CLRF RECTANGLE_HIGH
   
-
     GOTO MAIN
 
     ; ______________________________________________________________________
@@ -87,7 +92,6 @@ IRQ_HANDLE
 AD_CONVERSION
     BCF PIR1, ADIF ; Init back ADIF to 0x00
     MOVFF ADRESH, PORTB ; Start the conversion here ..
-    MOVF ADRESH, W ; Put the result inside the W for the display
     RETFIE
 
     ; Delay function that use the global variable COUNT
@@ -161,10 +165,10 @@ DIVISION
     MOVLW 0x00 ; To modify as an error
     MOVWF DIVISION_RESULT ; F = W
     RETURN
+    
     ; Check here the Num > Den
     ; CPFSGT Compare F with WREG / Skip if F > W
     CHECK_NUM_DEN
-    
     MOVFF DIVISION_DENOMINATOR, WREG ; DENOMINATOR => W
     CPFSGT REGA ; REGA > WREG ? / NUMERATOR > DENOMINATOR ?
     GOTO FINISH ; No
@@ -176,13 +180,13 @@ DIVISION
     
     DIVISION_LOOP
     MOVF DIVISION_DENOMINATOR, WREG ; Loading DIVISION_DENOMINATOR inside the WREG
-    SUBWF REGA, W ; WREG - REGA => WREG
-    BTFSC STATUS, N ; Negatif ? Skip if Clear // PROBLEM HERE
+    SUBWF REGA, W ; f - w => WREG / WREG - REGA => WREG
+    BTFSC STATUS, N ; Negatif ? Skip if Clear // PROBLEM HERE 
     RETURN ; Yes
     INCF DIVISION_RESULT, F ; No
     MOVWF REGA ; WREG => REGA for the new calculus in the next iteration
     GOTO DIVISION_LOOP
-    
+   
 MODULO
     ; REGA = DIVISION_RESULT
     ; MODULO_LOOP
@@ -217,13 +221,48 @@ MODULO
     SUBWF REGA ; REGA - W = DIVISION_NUMERATOR - REGB => REGA
     MOVFF REGA, DIVISION_MODULO
     RETURN
+    
+RECTANGULAR_SIGNAL
+    ; On recupère le signal de ADRESH => WREG
+    ; BEGIN
+    ; On DECF la valeur de WREG
+    ; Si 0 => Changement d'état du signal
+    ; GOTO BEGIN
+    ; Changer l'état du signal
+    ; Si 1 = 0
+    ; Si 0 = 1
+    ; RETURN
+    MOVFF ADRESH, ADRESSH_TO_DECREMENT
+    START_DECR
+    DECF ADRESSH_TO_DECREMENT
+    MOVF 0x00 ; We move 0x00 to WREG
+    CPFSEQ ADRESSH_TO_DECREMENT ; Skip if W = F
+    GOTO START_DECR; F != W
+    MOVF 0x00 ; F = W
+    CPFSEQ RECTANGLE_HIGH
+    GOTO PUT_LOW; On met à l'état bas ici
+    MOVLW 0x01; On met à l'état haut ici
+    MOVWF RECTANGLE_HIGH
+    GOTO TERMINATE
+    PUT_LOW
+    MOVLW 0x00
+    BCF PORTE, RE0
+    TERMINATE
+    BSF PORTE, RE0
+    RETURN
 
 MAIN
-    MOVLW 0xFE ; 1000
-    MOVWF DIVISION_NUMERATOR
-    MOVLW 0x7B ; 300
-    MOVWF DIVISION_DENOMINATOR
-    CALL DIVISION ; 3
-    CALL MODULO ;  100
+    BSF ADCON0, GO/DONE
+    CALL RECTANGULAR_SIGNAL
+    
+    ;MOVLW 0x34 ; 51
+    ;MOVWF DIVISION_NUMERATOR
+    ;MOVLW 0x03 ; 3
+    ;MOVWF DIVISION_DENOMINATOR
+    ;CALL DIVISION ; 16
+    ;CALL MODULO ;  2
+    
+    CALL _DISPLAY
+
     GOTO MAIN
     END
